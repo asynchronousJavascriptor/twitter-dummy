2c46a1c517bd0ff54034cd617084ca99479d9b6d
Harsh Sharma <harshu8545@gmail.com>
2024-11-20 20:28:03 +0530

    till 20th nov

---
+++ app.js
@@ -0,0 +1,88 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const userModel = require("./models/user.model");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const app = express();
+
+require("./config/db.config");
+
+app.set('view engine', 'ejs');
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.get("/", function (req, res) {
+    res.render("login");
+});
+
+app.get("/profile", isLoggedIn, function (req, res) {
+    
+});
+
+app.get("/register", function (req, res) {
+    res.render("register");
+});
+
+app.post("/register", function (req, res) {
+    let { username, password, email } = req.body;
+
+    bcrypt.genSalt(10, function (err, salt) {
+        bcrypt.hash(password, salt, async function (err, hash) {
+            await userModel.create({
+                username,
+                email,
+                password: hash
+            })
+
+            let token = jwt.sign({ email }, "secret"); // don't do this, extremely unsafe, for representational purpose only.
+            res.cookie("token", token);
+            res.send("created");
+        })
+    })
+
+});
+
+app.get("/login", function (req, res) {
+    res.render("login");
+})
+
+app.post("/login", async function (req, res) {
+    let { username, password } = req.body;
+    let user = await userModel.findOne({ username });
+    if (!user) return res.send("incorrect username or password");
+
+    bcrypt.compare(password, user.password, function (err, result) {
+        if (result) {
+            let token = jwt.sign({ email }, "secret"); // don't repeat this, extremely unsafe
+            res.cookie("token", token);
+            res.send("loggedin");
+        }
+    });
+})
+
+app.get("/logout", function (req, res) {
+    res.cookie("token", "");
+    res.redirect("/login");
+})
+
+function isLoggedIn(req, res, next) {
+    if (!req.cookies.token) return res.redirect("/login");
+    jwt.verify(req.cookies.token, "secret", function (err, decoded) {
+        if (err) {
+            res.cookie("token", "");
+            return res.redirect("/login");
+        }
+        else {
+            req.user = decoded;
+            next();
+        }
+    }) // don't write secret here, it's extremely unsafe
+
+}
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
